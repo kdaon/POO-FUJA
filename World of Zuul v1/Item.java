c80f0a837a43a7a7951b2a2c399c207b442d56fe
Gabriela Silva Memento <gabrielamemento@gmail.com>
2023-12-01 11:39:29 -0300

    Create Item.java

---
+++ World of Zuul v1/Item.java
@@ -0,0 +1,49 @@
+public class Item  {
+    private String nome;
+    private String descricao;
+    private boolean carregavel;
+    //String caminho;
+
+    public Item(String nome, String descricao, String caminho, boolean carregavel) {
+       //super(caminho);
+        this.nome = nome;
+        this.descricao = descricao;
+        this.carregavel = carregavel;
+        //this.caminho= caminho;
+    }
+
+    public boolean chave() {
+        return false;
+    }
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    // retorna se o objeto é carregavel
+    public boolean carregavel() {
+        return carregavel;
+    }
+
+    // retorna se o objeto é Arma
+    public boolean arma() {
+        return false;
+    }
+
+    // retorna se o objeto é kit medico
+    public boolean kitMedico() {
+        return false;
+    }
+
+    //retorna se é taco
+        public boolean taco() {
+        return false;
+    }
+        
+
+}
