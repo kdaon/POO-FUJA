89d7082890da48675f0f7ffd5c585bb2b361ffd2
Gabriela Silva Memento <gabrielamemento@gmail.com>
2023-12-03 19:56:15 -0300

    colocando em javaDoc

--- Jogo - Fuja/Jogo.java
+++ Jogo - Fuja/Jogo.java
@@ -2,7 +2,7 @@
 /**
  *  Essa classe cria e inicializa todas as demais classes do jogo, e foi feita com base no codigo base do jogo World of Zuul, de  Michael Kölling and David J. Barnes (traduzido por Julio Cesar Alves)
  *  
- * @author  Alexandra Melo, 
+ * @author  Alexandra Melo, Gabriela Memento
  * 
  */
 import java.util.Scanner;
