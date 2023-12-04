f390f462353d67d26f17974d24478f9b91b48b17
Olivia Campos <olivia.campos@estudante.ufla.br>
2023-12-03 22:18:09 -0300

    Comentando classe Inimigo

--- Jogo - Fuja/Inimigo.java
+++ Jogo - Fuja/Inimigo.java
@@ -11,11 +11,14 @@ public class Inimigo extends ObjetoDoJogo {
         super(nome, descricao);
         this.itemDerrotar = itemDerrotar;
     }
+
+    // Método que simula a derrota do inimigo, exibindo uma mensagem informando o nome do inimigo derrotado e o nome do item usado para derrotá-lo
     public void serDerrotado() {
         System.out.println("Você derrotou o " + getNome() + " usando " + itemDerrotar.getNome() + ".");
         itemDerrotar.usar();
     }
 
+    // Retorna o item necessário para derrotar o inimigo
     public Item getItemDerrotar() {
         return itemDerrotar;
     }
